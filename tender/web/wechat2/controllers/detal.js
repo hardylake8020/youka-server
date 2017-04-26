10817b7efce6ea5a7685063e73ea6e8ebce9374e
zenghongyujia123 <1963968619@qq.com>
2017-04-27 01:17:01 +0800

    Update fot test

--- tender/web/wechat2/controllers/detal.js
+++ tender/web/wechat2/controllers/detal.js
@@ -1,10 +1,30 @@
 /**
  * Created by zenghong on 2017/4/26.
  */
+var baseUrl = 'http://' + window.location.host;
+
 $(function () {
   $('.detail-back').click(function () {
     window.history.back();
   });
-
+  
+  
+  
   console.log(window.localStorage.getItem('tender_id'));
+  var tenderId = window.localStorage.getItem('tender_id');
+  getTenderDetail();
+  function getTenderDetail(){
+    $.ajax({
+      url:baseUrl+'/tender/driver/getTenderByTenderId',
+      method:'post',
+      data:{tender_id:tenderId},
+      success:function (data) {
+        console.log(data);
+        if(!data.err){
+          
+        }
+      }
+    });
+  }
+
 });
