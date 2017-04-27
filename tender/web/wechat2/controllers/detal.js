650761752c32df1a199df979132345aa591fa7d8
zenghongyujia123 <1963968619@qq.com>
2017-04-28 05:44:27 +0800

    Update for test

--- tender/web/wechat2/controllers/detal.js
+++ tender/web/wechat2/controllers/detal.js
@@ -8,23 +8,62 @@ $(function () {
     window.history.back();
   });
   
-  
-  
+  var list = $('.list-item');
+
+
   console.log(window.localStorage.getItem('tender_id'));
   var tenderId = window.localStorage.getItem('tender_id');
   getTenderDetail();
-  function getTenderDetail(){
+  function getTenderDetail() {
     $.ajax({
-      url:baseUrl+'/tender/user/getTenderByTenderId',
-      method:'post',
-      data:{tender_id:tenderId},
-      success:function (data) {
+      url: baseUrl + '/tender/user/getTenderByTenderId',
+      method: 'post',
+      data: {tender_id: tenderId},
+      success: function (data) {
         console.log(data);
-        if(!data.err){
-          
+        if (!data.err) {
+          var str =
+            '<div class="item-row">' +
+            '<div class="row-item title-col">' +
+            '首款金额' +
+            '</div>' +
+            '<div class="row-item price-col">' +
+            data.real_pay_top_cash +
+            '</div>' +
+            '<div class="row-item tiaozhang-col">' +
+            getTiaozhangStr(data.real_pay_top_tiaozhangs) +
+            '</div>' +
+            '<div class="verify-btn">' +
+            '通过' +
+            '</div>' +
+            '<div class="verify-btn pass">' +
+            '通过' +
+            '</div>' +
+            '</div>';
+
+          list.append(str);
         }
+        
+        
       }
     });
   }
 
+  function getTiaozhangStr(items) {
+    str = '';
+
+    items = items || [];
+    items.forEach(function (item) {
+      str +=
+        '<div class="tiaozhang-row">' +
+        '<div class="price">' +
+        item.price +
+        '</div>' +
+        '<div class="description">' +
+        item.reason +
+        '</div>' +
+        '</div>';
+    });
+    return str;
+  }
 });
