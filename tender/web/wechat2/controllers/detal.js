d21d107f54a2e6cb78ad20712f00d64d89e2c99d
zenghongyujia123 <1963968619@qq.com>
2017-04-28 06:18:41 +0800

    Update for test

--- tender/web/wechat2/controllers/detal.js
+++ tender/web/wechat2/controllers/detal.js
@@ -7,7 +7,7 @@ $(function () {
   $('.detail-back').click(function () {
     window.history.back();
   });
-  
+
   var list = $('.list-item');
 
 
@@ -22,93 +22,137 @@ $(function () {
       success: function (data) {
         console.log(data);
         if (!data.err) {
-          var str =
-            '<div class="item-row">' +
-            '<div class="row-item title-col">' +
-            '首款金额' +
-            '</div>' +
-            '<div class="row-item price-col">' +
-            data.real_pay_top_cash +
-            '</div>' +
-            '<div class="row-item tiaozhang-col">' +
-            getTiaozhangStr(data.real_pay_top_tiaozhangs) +
-            '</div>' +
-            '<div class="verify-btn">' +
-            '通过' +
-            '</div>' +
-            '<div class="verify-btn pass">' +
-            '通过' +
-            '</div>' +
-            '</div>';
+          var top =
+            $('<div class="item-row">' +
+              '<div class="row-item title-col">' +
+              '首款金额' +
+              '</div>' +
+              '<div class="row-item price-col">' +
+              data.real_pay_top_cash +
+              '</div>' +
+              '<div class="row-item tiaozhang-col">' +
+              getTiaozhangStr(data.real_pay_top_tiaozhangs) +
+              '</div>' +
+              '<div class="verify-btn pass">' +
+              '已通过' +
+              '</div>' +
+              '</div>');
 
-          list.append(str);
+          if (!data.real_pay_top_cash_time) {
+            top.find('.verify-btn').removeClass('pass').text('通过');
+            top.find('.verify-btn').click(function () {
+              payment('real_pay_top_cash');
+            });
+          }
 
-          var str =
-            '<div class="item-row">' +
-            '<div class="row-item title-col">' +
-            '尾款金额' +
-            '</div>' +
-            '<div class="row-item price-col">' +
-            data.real_pay_tail_cash +
-            '</div>' +
-            '<div class="row-item tiaozhang-col">' +
-            getTiaozhangStr(data.real_pay_tail_tiaozhangs) +
-            '</div>' +
-            '<div class="verify-btn">' +
-            '通过' +
-            '</div>' +
-            '<div class="verify-btn pass">' +
-            '通过' +
-            '</div>' +
-            '</div>';
+          list.append(top);
 
-          list.append(str);
+          var tail =
+            $('<div class="item-row">' +
+              '<div class="row-item title-col">' +
+              '尾款金额' +
+              '</div>' +
+              '<div class="row-item price-col">' +
+              data.real_pay_tail_cash +
+              '</div>' +
+              '<div class="row-item tiaozhang-col">' +
+              getTiaozhangStr(data.real_pay_tail_tiaozhangs) +
+              '</div>' +
+              '<div class="verify-btn">' +
+              '通过' +
+              '</div>' +
+              '<div class="verify-btn pass">' +
+              '通过' +
+              '</div>' +
+              '</div>');
 
-          var str =
-            '<div class="item-row">' +
-            '<div class="row-item title-col">' +
-            '回单金额' +
-            '</div>' +
-            '<div class="row-item price-col">' +
-            data.real_pay_last_cash +
-            '</div>' +
-            '<div class="row-item tiaozhang-col">' +
-            getTiaozhangStr(data.real_pay_last_tiaozhangs) +
-            '</div>' +
-            '<div class="verify-btn">' +
-            '通过' +
-            '</div>' +
-            '<div class="verify-btn pass">' +
-            '通过' +
-            '</div>' +
-            '</div>';
+          if (!data.real_pay_tail_cash_time) {
+            top.find('.verify-btn').removeClass('pass').text('通过');
+            top.find('.verify-btn').click(function () {
+              payment('real_pay_tail_cash');
+            });
+          }
 
-          list.append(str);
+          list.append(tail);
 
-          var str =
-            '<div class="item-row">' +
-            '<div class="row-item title-col">' +
-            '押金金额' +
-            '</div>' +
-            '<div class="row-item price-col">' +
-            data.real_pay_ya_jin_cash +
-            '</div>' +
-            '<div class="row-item tiaozhang-col">' +
-            getTiaozhangStr(data.real_pay_ya_jin_tiaozhangs) +
-            '</div>' +
-            '<div class="verify-btn">' +
-            '通过' +
-            '</div>' +
-            '<div class="verify-btn pass">' +
-            '通过' +
-            '</div>' +
-            '</div>';
+          var last =
+            $('<div class="item-row">' +
+              '<div class="row-item title-col">' +
+              '回单金额' +
+              '</div>' +
+              '<div class="row-item price-col">' +
+              data.real_pay_last_cash +
+              '</div>' +
+              '<div class="row-item tiaozhang-col">' +
+              getTiaozhangStr(data.real_pay_last_tiaozhangs) +
+              '</div>' +
+              '<div class="verify-btn">' +
+              '通过' +
+              '</div>' +
+              '<div class="verify-btn pass">' +
+              '通过' +
+              '</div>' +
+              '</div>');
 
-          list.append(str);
+          if (!data.real_pay_last_cash_time) {
+            top.find('.verify-btn').removeClass('pass').text('通过');
+            top.find('.verify-btn').click(function () {
+              payment('real_pay_last_cash');
+            });
+          }
+
+          list.append(last);
+
+          var ya_jin =
+            $('<div class="item-row">' +
+              '<div class="row-item title-col">' +
+              '押金金额' +
+              '</div>' +
+              '<div class="row-item price-col">' +
+              data.real_pay_ya_jin_cash +
+              '</div>' +
+              '<div class="row-item tiaozhang-col">' +
+              getTiaozhangStr(data.real_pay_ya_jin_tiaozhangs) +
+              '</div>' +
+              '<div class="verify-btn">' +
+              '通过' +
+              '</div>' +
+              '<div class="verify-btn pass">' +
+              '通过' +
+              '</div>' +
+              '</div>');
+
+          if (!data.real_pay_ya_jin_cash_time) {
+            top.find('.verify-btn').removeClass('pass').text('通过');
+            top.find('.verify-btn').click(function () {
+              payment('real_pay_ya_jin_cash');
+            });
+          }
+
+          list.append(ya_jin);
         }
-        
-        
       }
+    });
+  }
+
+  function payment(type, tenderId) {
+    $.ajax({
+      url: baseUrl + '/tender/user/payment',
+      data: {
+        type: type,
+        tender_id: tenderId
+      },
+      method: 'post',
+      success: function (data) {
+        if (!data.err) {
+          alert('操作成功！')
+          window.location = window.location;
+        }
+        else {
+          alert(data.err.type);
+        }
+      }
+
     });
   }
 
@@ -128,8 +172,8 @@ $(function () {
         '</div>';
     });
 
-    if(!str){
-      str='<div class="tiaozhang-row">' +
+    if (!str) {
+      str = '<div class="tiaozhang-row">' +
         '<div class="price">' +
         '无' +
         '</div>' +
